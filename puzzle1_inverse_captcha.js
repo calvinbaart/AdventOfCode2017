4b560ce2dff7bcc53104f20de0d89ca22b6d7fee
Calvin Baart <calvinbaart@users.noreply.github.com>
2017-12-01 13:48:07 +0100

    Update puzzle1_inverse_captcha.js

--- puzzle1_inverse_captcha.js
+++ puzzle1_inverse_captcha.js
@@ -1,26 +1,28 @@
 function captcha_part1(input) {
-    var num = 0;
+    let num = 0;
 
-    for(var i = 0;i<input.length;i++) {
-        var c = input.charAt(i);
-        var t = input.charAt((i + 1) % input.length);
+    for (let i = 0; i < input.length; i++) {
+        const c = input.charAt(i);
+        const t = input.charAt((i + 1) % input.length);
 
-        if (c === t)
+        if (c === t) {
             num += parseInt(c);
+        }
     }
 
     console.log(num);
 }
 
 function captcha_part2(input) {
-    var num = 0;
+    let num = 0;
 
-    for(var i = 0;i<input.length;i++) {
-        var c = input.charAt(i);
-        var t = input.charAt((i + (input.length / 2)) % input.length);
+    for (let i = 0; i < input.length; i++) {
+        const c = input.charAt(i);
+        const t = input.charAt((i + (input.length / 2)) % input.length);
 
-        if (c === t)
+        if (c === t) {
             num += parseInt(c);
+        }
     }
 
     console.log(num);
