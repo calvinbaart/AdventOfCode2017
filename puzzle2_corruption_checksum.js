873370886db18166c6956d6c79ecc9fe5ed87bf9
Calvin Baart <calvinbaart@users.noreply.github.com>
2017-12-02 13:05:11 +0100

    Update puzzle2_corruption_checksum.js

--- puzzle2_corruption_checksum.js
+++ puzzle2_corruption_checksum.js
@@ -46,3 +46,41 @@ function day2_part2(input) {
 
     console.log(sum);
 }
+
+// Alternative version
+function day2_part1(input) {
+    let sum = 0;
+
+    input.split("\n")
+         .forEach(x => {
+             let min = Number.MAX_VALUE;
+             let max = Number.MIN_VALUE;
+
+             x.split("\t")
+              .map(Number)
+              .forEach(x => { min = Math.min(min, x), max = Math.max(max, x) });
+            
+             sum += max - min;
+         });
+
+    console.log(sum);
+}
+
+function day2_part2(input) {
+    let sum = input
+        .split("\n")
+        .map(x => x
+            .split("\t")
+            .map(Number)
+            .map((y, _, a) => a
+                .map(x => y / Number(x))
+                .filter(x => Math.round(x) === x)
+                .reduce((x, y) => Math.max(x, y))
+            )
+            .filter(x => x !== 1)
+            .pop()
+        )
+        .reduce((a, b) => a + b);
+    
+    console.log(sum);
+}
